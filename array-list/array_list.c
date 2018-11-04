f05c0107e1b4bc0a44770cbf9ca795d6dd2d1388
Brian Morgan <brianmorgan2013@gmail.com>
2018-11-04 02:07:02 +0000

    arrayList

--- array-list/array_list.c
+++ array-list/array_list.c
@@ -2,7 +2,7 @@
 
 struct array_list* al_create(void){
     struct array_list* a = (struct array_list*)malloc(sizeof(struct array_list));
-    int* arr = (int*)malloc(sizeof(int * INITIAL_SIZE));
+    int* arr = (int*)malloc(sizeof(int) * INITIAL_SIZE);
     a->array = arr;
     a->size = 0;                                        //used as index for value entry
     a->capacity = INITIAL_SIZE;
@@ -14,32 +14,46 @@ void al_add(struct array_list* list, int value){
     int newCapacity, i;
     if(list->capacity == list->size){                    //adding element to size + 1
         newCapacity = list->capacity * 1.5;
-        int* newArr = (int*)malloc(int * newCapacity);
+        int* newArr = (int*)malloc(sizeof(int) * newCapacity);
         for(i = 0; i < list->capacity; i++){
-            newArr[i] = list->arr[i];
+            newArr[i] = list->array[i];
         }
-        list->arr = newArr;
+        list->array = newArr;
         list->capacity = newCapacity;
     }
     list->array[list->size] = value;
 }
 
 int al_get(struct array_list* list, int index){
-
+    if(index > list->capacity){                         //return -1 if index greater than current index
+        return -1;
+    }
+    return (list->array[index]);
 }
 
 void al_set(struct array_list* list, int index, int value){
-
+    if(index > list->capacity){
+        return -1;                                      //return -1 if index to add greater than current index
+    }
+    list->array[index] = value;
 }
 
 void al_remove(struct array_list* list, int index){
-
+    int i, temp;
+    if(index > list->capacity){
+        return -1;                                      //return -1 if index to remove is greater than capacity
+    }
+    list->array[index] = 0;                             //delete element, set to 0
+    for( i = index; i < list->capacity; i++){
+        temp = list->array[i+1];
+        list->array[i] = temp;
+    }
 }
 
 int al_size(struct array_list* list){
-
+    return list->size;
 }
 
 void al_destroy(struct array_list* list){
-
+    free();
 }
