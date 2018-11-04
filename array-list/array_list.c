a2e6ec5722a40ee595a174ba239b588df52a1bc4
bmorgan365 <brianmorgan2013@gmail.com>
2018-11-03 22:23:09 -0400

    Work in Progress

--- array-list/array_list.c
+++ array-list/array_list.c
@@ -1,4 +1,5 @@
 #include "array_list.h"
+#include <stdlib.h>
 
 struct array_list* al_create(void){
     struct array_list* a = (struct array_list*)malloc(sizeof(struct array_list));
@@ -32,17 +33,17 @@ int al_get(struct array_list* list, int index){
 }
 
 void al_set(struct array_list* list, int index, int value){
-    if(index > list->capacity){
-        return -1;                                      //return -1 if index to add greater than current index
-    }
+    //if(index > list->capacity){
+      //  return -1;                                      //return -1 if index to add greater than current index
+    
     list->array[index] = value;
 }
 
 void al_remove(struct array_list* list, int index){
     int i, temp;
-    if(index > list->capacity){
-        return -1;                                      //return -1 if index to remove is greater than capacity
-    }
+    // if(index > list->capacity){
+    //     return -1;                                      //return -1 if index to remove is greater than capacity
+    // }
     list->array[index] = 0;                             //delete element, set to 0
     for( i = index; i < list->capacity; i++){
         temp = list->array[i+1];
@@ -55,5 +56,8 @@ int al_size(struct array_list* list){
 }
 
 void al_destroy(struct array_list* list){
-    free();
+   free(list->array);
+   //free(list->size);
+   //free(list->capacity);
+   free(list);
 }
--- array-list/test.c
+++ array-list/test.c
@@ -3,6 +3,14 @@
 #include "array_list.h"
 
 int main(void){
-        struct array_list a* = al_create();
+        struct array_list* a = al_create();
+        int i;
+        al_add(a, 4);
+        al_size(a);
+        al_remove(a, 0);
+        al_add(a, 54);
+        for(i = 0; i < al_size(a); i++){
+                printf("The value at index %d is %d.", i, al_get(a, i));
+        }
         return 0;
 }
